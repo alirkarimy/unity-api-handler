f6780b3668405ee3353ef77666e49222fd0056a9
Alireza <ali.r.karimy@gmail.com>
2025-04-16 14:45:23 +0330

    move sources to package folder

---
+++ Assets/Samples/API Handler/1.0.0/Basic API Example/Scripts/SDK/AlecListener.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using Alec.Api;
+namespace Alec.Api
+{
+    public class AlecListener
+    {
+        /// <summary>
+        /// Occures when WebCore initializations input params receive correctly
+        /// </summary>
+        public static Action WebCoreInitialized;
+        public static Action<string> WebCoreInitializeFailed;
+
+        public static Action<string> OnUserRegisterRecived;
+        public static Action<ResponseStatus, string> OnUserRegisterFailed;
+
+        public static Action<LoginModel> OnLoginRecived;
+        public static Action<ResponseStatus, string> OnLoginFailed;
+
+        public static Action<GetUserProfileApiModel> OnGetUserProfileRecived;
+        public static Action<ResponseStatus, string> OnGetUserProfileFailed;
+
+        public static Action<string> OnUpdatePlayerRecived;
+        public static Action<ResponseStatus, string>  OnUpdatePlayerFailed;
+
+
+    public static Action OnTokenExpired;
+
+    }
+}
